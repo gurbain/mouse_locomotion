f13ba35e21aa34fbf0c0622de6609e45eb14da7c
Gabriel Urbain <gabriel.urbain@gmail.com>
2016-04-27 16:56:12 +0200

    Add Genetic algoithm for brain routing optimization (experimental)

--- src/sim.py
+++ src/sim.py
@@ -22,6 +22,9 @@ import fcntl
 import logging
 import os
 import pickle
+import pyevolve
+from pyevolve import *
+import logging
 import socket
 import struct
 import subprocess
@@ -110,39 +113,103 @@ class Simulation:
         logging.info("Results: " + str(rs_ls))
         logging.info("Simulation Finished!")
 
+    def __create_ga(self):
+        """Creation and initialization function for the genome and the genetic algorithm. It fixes the
+        parameters to use in the algorithm"""
+
+        # Algo parameters
+        self.genome_size = 10
+        self.population_size = 20
+        self.num_max_generation = 50
+        self.mutation_rate = 0.2
+        self.cross_over_rate = 0.9
+        self.genome_min = 0
+        self.genome_max = 1.0
+        self.interactive_mode = False
+        self.initializator = Initializators.G1DListInitializatorReal
+        self.mutator = Mutators.G1DListMutatorRealGaussian
+        self.selector = Selectors.GTournamentSelector
+        self.stop_num_av = 10
+        self.stop_thresh = 0.01
+        self.bf_list = []
+
+        # Create a genome instance and parametrize it
+        genome = G1DList.G1DList(self.genome_size)
+        genome.evaluator.set(self.__eval_fct)
+        genome.initializator.set(self.initializator)
+        genome.mutator.set(self.mutator)
+        genome.setParams(rangemin=self.genome_min, rangemax=self.genome_max)
+
+        # Create a Genetic Algorithm (ga) instance and parametrize it
+        ga = GSimpleGA.GSimpleGA(genome)
+        ga.selector.set(self.selector)
+        ga.setGenerations(self.num_max_generation)
+        ga.setMutationRate(self.mutation_rate)
+        ga.setPopulationSize(self.population_size)
+        ga.setCrossoverRate(self.cross_over_rate)
+        ga.setInteractiveMode(self.interactive_mode)
+        ga.terminationCriteria.set(self.__conv_fct)
+
+        # Return the algorithm instance
+        return ga
+
+    def __eval_fct(self, genome):
+        """Evaluation function of the genetic algorithm. For each set of genome, it compute a 
+        score and returns it"""
+
+        score = 0.0
+
+        # Create a config for the genome
+        self.opt["genome"] = genome.getInternalList()
+        logging.info(" ---------------- DEBUT SIM -----------")
+
+        # Simulate
+        sim_list = [self.opt]
+        res_list = self.sm.simulate(sim_list)
+
+        # In the result list, we look for the score
+        score = 1
+        logging.info(" ---------------- FIN SIM -----------")
+
+        # Return the score result
+        return score
+
+    def __conv_fct(self, ga):
+        """Convergence function of the genetic algorithm. It is called at each iteration step and 
+        return True of Flase depending on a convergence criteria"""
+
+        # Get best individus
+        pop = ga.getPopulation()
+        bi = pop.bestFitness()
+        self.bf_list.append(bi.getFitnessScore())
+
+        # Return the convergence
+        if len(self.bf_list) > self.stop_num_av:
+            av = sum(self.bf_list[-self.stop_num_av:]) / self.stop_num_av
+            #print("av: " + str(av)  + " curr: " + str(self.bf_lis[-1]) + ' abs: ' + str(abs(self.bf_lis[-1] - av)))
+            if  abs(self.bf_list[-1] - av) < self.stop_thresh:
+                logging.info("Criterion reached. Best element : " +  str(bi.getInternalList()))
+                return True
+
+        return False
+
     def brain_opti_sim(self):
         """Run an iterative simulation to optimize the muscles parameters"""
-
-        # Set-up simulator options
-        stop_loop = False
-        n_iter = 0
 
         # Start manager
         self.start_manager()
 
-        # Offline optimization loop
-        while not stop_loop:
+        # Create genetic algorithm
+        ga = self.__create_ga()
 
-            # Create a population
+        # Run genetic algorithm until convergence or max iteration reached
+        ga.evolve(freq_stats=10)
 
-            # Append the population to the sim list
-            sim_list = [self.opt]
-
-            # Run the simulation
-            res_list = self.sm.simulate(sim_list)
-
-            # Exit condition is triggered in main.py
-
-            # Check cost function and modify config if needed
-            if n_iter >= 0:
-                stop_loop = True
-            n_iter += 1
-
-        # Stop and display results
-        sys.stdout.write("Resultats: ")
-        for i in res_list:
-            sys.stdout.write(str(i) + " ")
+        # Stop and disply results
         logging.info("Simulation Finished!")
+        self.stop_manager()
+        time.sleep(1)
+        logging.info(ga.bestIndividual())
 
     def muscle_opti_sim(self):
         """Run an iterative simulation to optimize the muscles parameters"""
